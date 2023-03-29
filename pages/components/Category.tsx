3635f3576ee0ce7e6621af0db17ac5b4182f9121
Nitin Yadav <nitin.yadav.ny237@gmail.com>
2023-03-29 20:41:09 +0530

    first commit

--- pages/components/Category.tsx
+++ pages/components/Category.tsx
@@ -1,69 +1,72 @@
-import React from 'react'
-import {FaStaylinked,FaArrowRight } from "react-icons/fa"
-type Props = {}
+import React from "react";
+import { FaStaylinked, FaArrowRight } from "react-icons/fa";
+type Props = {};
 
 const Category = (props: Props) => {
   return (
-    <div className='bg-white'>
-<div className="container mx-auto grid grid-cols-3 gap-[40px] py-[40px]">
+    <div className="bg-white">
+      <div className="container mx-auto grid grid-cols-3 gap-[40px] py-[40px]">
+        <div className="flex gap-[15px]">
+          <FaStaylinked fontSize={70} />
+          <div>
+            <h1 className="text-[24px] pt-[10px] font-semibold text-[#0E1D34]">
+              Lorem Ipsum
+            </h1>
+            <p className="text-[15px] font-normal leading-[23px] text-gray-600 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
+              laudantium fugit animi minima earum voluptas molestiae quod
+              aliquam nihil velit deleniti dicta id amet repellat. Hic ratione
+              quasi laboriosam veritatis.
+            </p>
 
-<div className='flex gap-[15px]'>
-    <FaStaylinked fontSize={70}/>
-    <div>
-    <h1 className="text-[24px] pt-[10px] font-semibold text-[#0E1D34]">Lorem Ipsum</h1>
-    <p className='text-[15px] font-normal leading-[23px] text-gray-600 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, laudantium fugit animi minima earum voluptas molestiae quod aliquam nihil velit deleniti dicta id amet repellat. Hic ratione quasi laboriosam veritatis.</p>
+            <div className="flex items-center gap-[7px] cursor-pointer mt-[10px] text=[14px] font-semibold">
+              <p className="text-gray-700">Learn More</p>
+              <FaArrowRight />
+            </div>
+          </div>
+        </div>
 
-      <div className='flex items-center gap-[7px] cursor-pointer mt-[10px] text=[14px] font-semibold'>
-        <p className='text-gray-700'>Learn More</p>
-        <FaArrowRight/>
+        <div className="flex gap-[15px]">
+          <FaStaylinked fontSize={70} />
+          <div>
+            <h1 className="text-[24px] pt-[10px] font-semibold text-[#0E1D34]">
+              Lorem Ipsum
+            </h1>
+            <p className="text-[15px] font-normal leading-[23px] text-gray-600 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
+              laudantium fugit animi minima earum voluptas molestiae quod
+              aliquam nihil velit deleniti dicta id amet repellat. Hic ratione
+              quasi laboriosam veritatis.
+            </p>
+
+            <div className="flex items-center gap-[7px] cursor-pointer mt-[10px] text=[14px] font-semibold">
+              <p className="text-gray-700">Learn More</p>
+              <FaArrowRight />
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-[15px]">
+          <FaStaylinked fontSize={70} />
+          <div>
+            <h1 className="text-[24px] pt-[10px] font-semibold text-[#0E1D34]">
+              Lorem Ipsum
+            </h1>
+            <p className="text-[15px] font-normal leading-[23px] text-gray-600 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
+              laudantium fugit animi minima earum voluptas molestiae quod
+              aliquam nihil velit deleniti dicta id amet repellat. Hic ratione
+              quasi laboriosam veritatis.
+            </p>
+
+            <div className="flex items-center gap-[7px] cursor-pointer mt-[10px] text=[14px] font-semibold">
+              <p className="text-gray-700">Learn More</p>
+              <FaArrowRight />
+            </div>
+          </div>
+        </div>
       </div>
-
     </div>
-</div>
+  );
+};
 
-
-<div className='flex gap-[15px]'>
-    <FaStaylinked fontSize={70}/>
-    <div>
-    <h1 className="text-[24px] pt-[10px] font-semibold text-[#0E1D34]">Lorem Ipsum</h1>
-    <p className='text-[15px] font-normal leading-[23px] text-gray-600 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, laudantium fugit animi minima earum voluptas molestiae quod aliquam nihil velit deleniti dicta id amet repellat. Hic ratione quasi laboriosam veritatis.</p>
-
-      <div className='flex items-center gap-[7px] cursor-pointer mt-[10px] text=[14px] font-semibold'>
-        <p className='text-gray-700'>Learn More</p>
-        <FaArrowRight/>
-      </div>
-
-    </div>
-</div>
-<div className='flex gap-[15px]'>
-    <FaStaylinked fontSize={70}/>
-    <div>
-    <h1 className="text-[24px] pt-[10px] font-semibold text-[#0E1D34]">Lorem Ipsum</h1>
-    <p className='text-[15px] font-normal leading-[23px] text-gray-600 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, laudantium fugit animi minima earum voluptas molestiae quod aliquam nihil velit deleniti dicta id amet repellat. Hic ratione quasi laboriosam veritatis.</p>
-
-      <div className='flex items-center gap-[7px] cursor-pointer mt-[10px] text=[14px] font-semibold'>
-        <p className='text-gray-700'>Learn More</p>
-        <FaArrowRight/>
-      </div>
-
-    </div>
-</div>
-
-
-
-
-    </div>
-
-</div>
-
-
-
-
-
-
-
-   
-  )
-}
-
-export default Category  
+export default Category;
